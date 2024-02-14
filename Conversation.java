0aa75efb6923e55d1d3dd55514f04540b7147838
Emily Ren <shuyueeren@gmail.com>
2024-02-13 19:35:20 -0500

    structuring

---
+++ Conversation.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Conversation {
+
+  public static String[] transcript;
+  public static String[] randomResponses = {"e","e"};
+
+  public static void main(String[] arguments) {
+    // You will start the conversation here.
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How many rounds? (in numbers)"); // Take input of how many rounds
+    int userRoundInput = sc.nextInt();
+    System.out.println("Hi, what's up?");
+    
+    for (int i=0; i < userRoundInput; i++){
+      String userInput = sc.nextLine();
+      if (userInput.contains("?")):
+        
+
+    } // Loop for number of rounds
+      // Greeting line (maybe variation)
+      // Take new user input
+      //if userInput.contains("I "):
+        // return "You" and userinput sentence
+        // if userInput.contains
+    
+    // print different messages
+        
+
+  }
+}
